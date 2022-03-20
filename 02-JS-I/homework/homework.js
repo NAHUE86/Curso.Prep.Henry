9ed0ac3aa14df3c80a7eb391b1e17413f958495e
Nahuel <nahuelnestorcastelar@gmail.com>
2022-03-20 16:02:41 -0300

    resueltos todos

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,11 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-<<<<<<< HEAD
-const nuevaString = "esto";
-=======
-const nuevaString = "hola"
->>>>>>> 536cb940840035036693849f2b4b8ac017678514
+const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 1;
@@ -14,23 +10,13 @@ const nuevoNum = 1;
 const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
-<<<<<<< HEAD
-const nuevaResta = 10 - 5 === 5;
+const nuevaResta = 10 - 5 == 5;
 
 // Resuelve el siguiente problema matemático:
 const nuevaMultiplicacion = 10 * 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 1;
-=======
-const nuevaResta = 10 - 5=== 5;
-
-// Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * 4=== 40 ;
-
-// Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === 1
->>>>>>> 536cb940840035036693849f2b4b8ac017678514
+const nuevoModulo = (21 % 5) === 1;
 
 
 // En los próximos 22 problemas, deberás completar la función.
